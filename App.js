8378c4f3868f5a5824b6ae51ff300096f5f5394a
jonan270 <jonan270@student.liu.se>
2022-11-16 09:16:32 +0100

    Add microship ontop of card bg

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import cardBg from './assets/images/card_bg.jpeg'
+import chipImg from './assets/images/chip.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
         source={cardBg}
-        style={styles.cardBgContainer}
+        style={styles.cardImageBg}
       >
-        <Text style={styles.cardText}>Test</Text>
+        <View style={styles.cardView}>
+          <Image source={chipImg} style={{width: 101*0.65, height: 82*0.65}}/>
+          <Text style={styles.cardText}>Test</Text>
+        </View>
       </ImageBackground>
       <Text>Open up App.js to start on your app!</Text>
       <StatusBar style="auto" />
@@ -29,10 +33,14 @@ const styles = StyleSheet.create({
     color: Colors.white,
     fontSize: 20,
   },
-  cardBgContainer: {
-    width: 270,
-    height: 174,
+  cardImageBg: {
+    width: 405,
+    height: 261,
     borderRadius: 15,
     overflow: "hidden",
+  },
+  cardView: {
+    flex: 1,
+    padding: 25,
   }
 });
