537adc80376f144642a7ea27cc48daf3b5a4d0aa
jonan270 <jonan270@student.liu.se>
2022-11-22 11:29:45 +0100

    Layout styling and input functionality

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
 import Card from './assets/components/Card.js'
 
@@ -11,21 +11,34 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.inputView}>
-        <TextInput
-          style={styles.input}
-          onChangeText={input => setCardNumber(input)}
-          placeholder="Card number"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={input => setCardHolder(input)}
-          placeholder="Card holder"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={input => setExpire(input)}
-          placeholder="Expires"
-        />
+        <View style={styles.inputRow}>
+          <Text style={styles.inputText}>Card Number</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType={'numeric'}
+            onChangeText={input => setCardNumber(input)}
+          />
+        </View>
+        <View style={styles.inputRow}>
+          <Text style={styles.inputText}>Card Name</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={input => setCardHolder(input)}
+          />
+        </View>
+        <View style={styles.inputRow}>
+          <Text style={styles.inputText}>Expiration date</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={input => setExpire(input)}
+          />
+        </View>
+        <View style={{width: '100%', margin: 15,}}>
+          <Button
+            title="SUBMIT"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
       </View>
       <Card
         cardHolder = {cardHolder}
@@ -47,20 +60,30 @@ const styles = StyleSheet.create({
     marginTop: 300,
     position: 'absolute',
     padding: 30,
-    height: '75%',
+    height: '85%',
     width: 300,
-    bottom: 100,
+    bottom: 0,
     borderRadius: 15,
     backgroundColor: '#ffffff',
     alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  inputRow : {
+    width: '100%',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputText : {
+    alignSelf: 'flex-start',
+    fontSize: 12,
   },
   input: {
-    borderColor: "gray",
+    borderColor: "#dbdbdb",
+    height: 35,
     width: "100%",
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
-    margin: 10,
+    margin: 5,
   },
 });
