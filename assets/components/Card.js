6e9dd4ff1f4513cb4880091b93876fb36faebb57
jonan270 <57438307+jonan270@users.noreply.github.com>
2022-11-24 17:12:43 +0100

    Update brand image based on input

--- assets/components/Card.js
+++ assets/components/Card.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import cardBg from '../images/card_bg.jpeg'
@@ -8,40 +9,85 @@ import chipImg from '../images/chip.png'
 
 import mastercardImg from '../images/mastercard.png'
 import visaImg from '../images/visa.png'
+import amexImg from '../images/amex.png'
+import discoverImg from '../images/discover.png'
+import troyImg from '../images/troy.png'
 
 const imgScaleFactor = 0.45;
 const topImgW = 100 * imgScaleFactor;
 const topImgH = 80 * imgScaleFactor;
 
-const showBackside = false;
-
 const transparentColor = 'hsla(360, 100%, 100%, 0.0)'
 const markerColor = 'hsla(360, 100%, 100%, 1.0)'
 
 export default function Card(props) {
+  
+  const [brandImg, setBrandImg] = useState(visaImg);
 
-    if(props.showBackside) {
-      return(
-        <View>
-          <ImageBackground
-            source={backsideBg}
-            style={styles.cardImageBg}
-          >
-              <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
-                <View style={styles.magnetStripView}></View>
-                <Text style={{color: Colors.white, alignSelf: 'flex-end', marginRight: '10%', marginTop: '3%'}}>CVV</Text>
-                <View style={styles.cvvView}><Text style={styles.cvvText}>{props.cvv}</Text></View>
-                <View style={{margin: 32 * imgScaleFactor}}>
-                  <Image source={visaImg} style={styles.brandImage}/>
-                </View>
-              </View>
-          </ImageBackground>
-          <StatusBar style="auto" />
-        </View>
-      )
+  function setCardType () {
+    let number = props.cardNumber;
+
+    let re = new RegExp("^4");
+    if (number.match(re) != null) {
+      setBrandImg(visaImg);
+      return;
     }
 
-    return (
+    re = new RegExp("^(34|37)");
+    if (number.match(re) != null) {
+      setBrandImg(amexImg);
+      return;
+    }
+
+    re = new RegExp("^5[1-5]");
+    if (number.match(re) != null) {
+      setBrandImg(mastercardImg);
+      return;
+    }
+
+    re = new RegExp("^6011");
+    if (number.match(re) != null) {
+      setBrandImg(discoverImg);
+      return;
+    }
+    
+    re = new RegExp('^9792')
+    if (number.match(re) != null) {
+      setBrandImg(troyImg);
+      return;
+    }
+
+    setBrandImg(visaImg); // default type
+  }
+
+  // When cardNumber is changed, update image used.
+  useEffect(() => {
+    setCardType();
+  },[props.cardNumber])
+
+
+  if(props.showBackside) {
+    return(
+      <View>
+        <ImageBackground
+          source={backsideBg}
+          style={styles.cardImageBg}
+        >
+            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
+              <View style={styles.magnetStripView}></View>
+              <Text style={{color: Colors.white, alignSelf: 'flex-end', marginRight: '10%', marginTop: '3%'}}>CVV</Text>
+              <View style={styles.cvvView}><Text style={styles.cvvText}>{props.cvv}</Text></View>
+              <View style={{margin: 32 * imgScaleFactor}}>
+                <Image source={brandImg} style={styles.brandImage}/>
+              </View>
+            </View>
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
+
+  return (
     <View>
         <ImageBackground
             source={cardBg}
@@ -50,7 +96,7 @@ export default function Card(props) {
           <View style={styles.cardView}>
               <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
               <Image source={chipImg} style={{width: topImgW, height: topImgH}}/>
-              <Image source={visaImg} style={styles.brandImage}/>
+              <Image source={brandImg} style={styles.brandImage}/>
               </View>
               {/* Mark border if cardNumber input is selected */}
               <View style={[
